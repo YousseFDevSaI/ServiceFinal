35ff0edfbfbb4a5f8508448f7cf46047481065ac
“youssef1234566” <121753059+YousseFDevSaI@users.noreply.github.com>
2024-05-24 18:00:39 +0300

    Desks Microservice Done

--- storage-microservices/src/desks/desks.controller.ts
+++ storage-microservices/src/desks/desks.controller.ts
@@ -1,16 +1,37 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { DesksService } from './desks.service';
+import { DesksSchema } from './schemas/desks.schema';
 
 
 @Controller('desks')
 export class DesksController {
 
-    constructor(private readonly DeskService: DesksService) {}
+    constructor(private readonly desksService: DesksService) {}
 
+    @Post()
+    async create(@Body() createDeskDto: any): Promise<DesksSchema> {
+      return this.desksService.create(createDeskDto);
+    }
+  
     @Get()
-    getData(): string {
-        return this.DeskService.getData();
+    async findAll(): Promise<DesksSchema[]> {
+      return this.desksService.findAll();
+    }
+  
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<DesksSchema> {
+      return this.desksService.findOne(id);
+    }
+  
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateDeskDto: any): Promise<DesksSchema> {
+      return this.desksService.update(id, updateDeskDto);
+    }
+  
+    @Delete(':id')
+    async delete(@Param('id') id: string): Promise<DesksSchema> {
+      return this.desksService.delete(id);
     }
 
 }
--- storage-microservices/src/desks/desks.service.ts
+++ storage-microservices/src/desks/desks.service.ts
@@ -1,9 +1,33 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DesksSchema } from './schemas/desks.schema';
 
 @Injectable()
 export class DesksService {
-    getData(): string {
-        return 'Data from the microservice: Desks';
-      }
+
+  constructor(@InjectModel(DesksSchema.name) private readonly deskModel: Model<DesksSchema>) {}
+
+
+  async create(deskData: any): Promise<DesksSchema> {
+    const createdDesk = new this.deskModel(deskData);
+    return createdDesk.save();
+  }
+
+  async findAll(): Promise<DesksSchema[]> {
+    return this.deskModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<DesksSchema> {
+    return this.deskModel.findById(id).exec();
+  }
+
+  async update(id: string, updatedDeskData: any): Promise<DesksSchema> {
+    return this.deskModel.findByIdAndUpdate(id, updatedDeskData, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<DesksSchema> {
+    return this.deskModel.findOneAndDelete({ _id: id }).exec();
+  }
 }
